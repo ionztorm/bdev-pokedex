d9292f281c9dda6fa3f785822e029aa9f3f5be38
Leon <coding@leonlonsdale.dev>
2025-06-29 11:52:59 +0100

    feat: tests for cleanInput()

---
+++ repl_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+type testCase struct {
+	input    string
+	expected []string
+}
+
+func TestCleanInput(t *testing.T) {
+	cases := []testCase{
+		{"TEST LOWER CASE 1", []string{"test", "lower", "case", "1"}},
+		{"TeSt LoWeR CaSe 2", []string{"test", "lower", "case", "2"}},
+		{"  Test preceeding white space", []string{"test", "preceeding", "white", "space"}},
+		{"Test post whitespace    ", []string{"test", "post", "whitespace"}},
+		{"", []string{}},
+		{"    ", []string{}},
+	}
+
+	for _, c := range cases {
+		output := cleanInput(c.input)
+		if len(output) != len(c.expected) {
+			t.Errorf("For input: %s\nExpected length of slice to be %d but got %d.", c.input, len(c.expected), len(output))
+		}
+
+		for i := range c.expected {
+			word := output[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("For input: '%s'\nAt index %d: Expected '%s' but got '%s'", c.input, i, c.expected[i], output[i])
+			}
+		}
+	}
+}
