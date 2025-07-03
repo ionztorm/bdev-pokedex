6c74fbb0f187aec862f6e6f795903551f6a9011b
Leon <coding@leonlonsdale.dev>
2025-07-03 21:02:36 +0100

    refactor: couple getRawDataFromURL() with Fetch()

--- internal/api/fetch.go
+++ internal/api/fetch.go
@@ -2,7 +2,11 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"pokedex/internal/pokecache"
+	"pokedex/internal/utils"
 )
 
 func Fetch(cache pokecache.Cacher, url string, target any) error {
@@ -16,4 +20,32 @@ func Fetch(cache pokecache.Cacher, url string, target any) error {
 		cache.Add(url, data)
 	}
 	return json.Unmarshal(data, target)
+}
+
+func getRawDataFromURL(endpoint string) ([]byte, error) {
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("arror fetching data: %w", err)
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("an error occurred: %w", err)
+	}
+
+	defer utils.SafeClose(res.Body)
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Println(res.StatusCode)
+		return nil, fmt.Errorf("problem with response")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	return body, nil
 }
