153ee7900b6302f37b705b72ed3efb20f363e47d
Leon <coding@leonlonsdale.dev>
2025-07-03 21:09:31 +0100

    refactor: move cleanInput() to utils.CleanInput()

--- cleaninput.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "strings"
-
-func cleanInput(text string) []string {
-	return strings.Fields(strings.TrimSpace(strings.ToLower(text)))
-}
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func SafeClose(c io.Closer) {
 	if err := c.Close(); err != nil {
 		fmt.Fprintf(os.Stderr, "error closing resource: %v\n", err)
 	}
+}
+
+func CleanInput(text string) []string {
+	return strings.Fields(strings.TrimSpace(strings.ToLower(text)))
 }
